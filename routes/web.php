fe1b874e5c04bc14cb1fb8cb8b1553c1513d916a
outnova <bjalastre@gmail.com>
2025-01-12 19:29:19 -0400

    fix: web.php routes emptied for now

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', [HomeController::class, 'index']);
+
+//Route::post('/users/exists', [UserController::class, 'checkUsername']);
+
+/*
+Route::get('/register', function () {
+    return "Formulario de registro";
 });
+
+Route::get('/login', function () {
+    return "Formulario de inicio de sesión";
+});
+*/
+
