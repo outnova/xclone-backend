c3308f42df4b85cc4f4ac5c46953d1f072f9cd41
outnova <bjalastre@gmail.com>
2025-01-13 13:42:07 -0400

    refactor: moved to api folder

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function checkUsername(Request $request)
+    {
+        $username = $request->input('name'); // Obtiene el parámetro del cuerpo
+        $exists = User::where('name', $username)->exists();
+    
+        return response()->json(['username' => $username, 'exists' => $exists]);
+    }
+    //
+}
