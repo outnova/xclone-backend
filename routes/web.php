05aca7f1403476b8d5d4e91597200c023d3b76fe
outnova <bjalastre@gmail.com>
2025-01-15 15:12:14 -0400

    perf: route '/' action HomeController:index

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-/*
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+/*
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 */
 
-//Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 
 //Route::post('/users/exists', [UserController::class, 'checkUsername']);
 
@@ -20,4 +21,3 @@ Route::get('/login', function () {
     return "Formulario de inicio de sesión";
 });
 */
-
