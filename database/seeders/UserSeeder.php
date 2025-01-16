ca9eae8cf2d635d119762b2c1620b37db64e09e6
outnova <bjalastre@gmail.com>
2025-01-16 16:41:33 -0400

    feat: add UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password123'),
+        ]);
+
+        User::create([
+            'name' => 'Jane Smith',
+            'email' => 'jane@example.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password123'),
+        ]);
+
+        User::create([
+            'name' => 'Bob Johnson',
+            'email' => 'bob@example.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password123'),
+        ]);
+        //
+    }
+}
