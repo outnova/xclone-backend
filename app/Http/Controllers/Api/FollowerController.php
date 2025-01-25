c88d1008a3e2b79f9d0d24943dab37de3201d39b
outnova <bjalastre@gmail.com>
2025-01-24 23:48:17 -0400

    feat: create Follower controller

---
+++ app/Http/Controllers/Api/FollowerController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class FollowerController extends Controller
+{
+    public function follow(Request $request, $followedId)
+    {
+        try {
+            $user = $request->user();
+
+            //validate if following user
+
+            if($user->following()->where('followed_id', $followedId)->exists()) {
+                return response()->json([
+                    'message' => 'Ya estás siguiendo a este usuario',
+                ], 400);
+            }
+
+            //validate if user to follow exists
+            $followedUser = User::find($followedId);
+            if(!$followedUser) {
+                return response()->json([
+                    'message' => 'El usuario que intentas seguir no existe',
+                ], 400);
+            }
+
+            //create follow relationship
+            $user->following()->attach($followedId);
+
+            return response()->json([
+                'message' => 'Ahora sigues a este usuario',
+            ], 201);
+        }
+        catch(\Throwable $th) {
+            return response()->json([
+                'message' => 'Ocurrió un error al intentar seguir al usuario',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function unfollow(Request $request, $followedId) {
+        try {
+            $user = $request->user();
+
+            //validate if following user
+
+            if(!$user->following()->where('followed_id', $followedId)->exists()) {
+                return response()->json([
+                    'message' => 'No estás siguiendo a este usuario',
+                ], 400);
+            }
+
+            //delete the relationship
+
+            $user->following()->detach($followedId);
+
+            return response()->json([
+                'message' => 'Has dejado de seguir a este usuario',
+            ], 200);
+        }
+        catch(\Throwable $th) {
+            return response()->json([
+                'message' => 'Ocurrió un error al intentar de seguir a este usuario',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
+    }
+    //
+}
