19f7db4447cac6154e94a0a74765c20dbd3eb4a0
outnova <bjalastre@gmail.com>
2025-01-25 15:32:17 -0400

    feat: add methods to get followers/following with count

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -15,5 +15,77 @@ class UserController extends Controller
     
         return response()->json(['username' => $username, 'exists' => $exists]);
     }
+
+    public function getFollowersCount($userId) 
+    {
+        try {
+            $user = User::findOrFail($userId);
+            $followerCount = $user->followers()->count();
+
+            return response()->json([
+                'user_id' => $user->id,
+                'follower_count' => $followerCount,
+            ], 200);
+        } catch(\Throwable $th) {
+            return response()->json([
+                'message' => 'Error al obtener la cantidad de seguidores',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function getFollowingCount($userId) 
+    {
+        try {
+            $user = User::findOrFail($userId);
+            $followingCount = $user->following()->count();
+
+            return response()->json([
+                'user_id' => $user->id,
+                'following_count' => $followingCount,
+            ], 200);
+        } catch(\Throwable $th) {
+            return response()->json([
+                'message' => 'Error al obtener la cantidad de seguidos',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function getFollowers($userId)
+    {
+        try {
+            $user = User::findOrFail($userId);
+            $followers = $user->followers()->get();
+
+            return response()->json([
+                'user_id' => $user->id,
+                'followers' => $followers,
+            ], 200);
+        } catch(\Throwable $th) {
+            return response()->json([
+                'message' => 'Error al obtener los seguidores',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function getFollowing($userId)
+    {
+        try {
+            $user = User::findOrFail($userId);
+            $following = $user->following()->get();
+
+            return response()->json([
+                'user_id' => $user->id,
+                'following' => $following,
+            ], 200);
+        } catch(\Throwable $th) {
+            return response()->json([
+                'message' => 'Error al obtener los seguidores',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
+    }
     //
 }
