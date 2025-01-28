2e0962d19e7a06434db7e26bef3a05e46348adb5
outnova <bjalastre@gmail.com>
2025-01-28 19:29:08 -0400

    feat: add files features (images/videos) to post (create/delete)

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
@@ -22,7 +23,7 @@ class PostController extends Controller
         try {
             $validated = $request->validate([
                 'content' => 'required|string|max:1000',
-                'media' => 'nullable|file|mimes:jpg,png,jpeg,gif|max:2048', //opcional
+                'files.*' => 'nullable|file|mimes:jpeg,png,jpg,gif,mp4,mov,avi,wmv|max:10240', //opcional
                 'visibility' => 'in:public,private,followers',
             ]);
 
@@ -31,14 +32,54 @@ class PostController extends Controller
             //create the post
             $post = Post::create([
                 'content' => $validated['content'],
-                'media' => $validated['media'] ?? null,
+                //'media' => $validated['media'] ?? null,
                 'visibility' => $validated['visibility'] ?? 'public',
                 'user_id' => $user->id,
             ]);
 
+            $uploadedFiles = [];
+
+            //updoad files (images - videos)
+            if($request->hasFile('files')) {
+                foreach($request->file('files') as $file) {
+
+                    //get the extesion of the file
+                    $extension = $file->getClientOriginalExtension();
+
+                    $fileType = DB::table('files_types')
+                        ->where('extension', $extension)
+                        ->first();
+
+                    if(!$fileType) {
+                        //if the extension isn't registered, throw a error
+                        return response()->json([
+                            'message' => "El tipo de archivo con extensión .$extension no está soportado."
+                        ], 400);
+                    }
+
+                    //generate a unique name for each file 
+                    $fileName = uniqid(time()) . '_' . '.' . $file->getClientOriginalExtension();
+
+                    //define the folder by file type (image or video)
+                    $folder = $fileType->type === 'image' ? 'images' : 'videos';
+
+                    $path = $file->storeAs("public/posts/{$post->id}/{$folder}", $fileName);
+
+                    //save the file in db
+                    $fileRecord = $post->files()->create([
+                        'path' => str_replace('public/', 'storage/', $path),
+                        'id_type' => $fileType->id,
+                        'id_post' => $post->id,
+                    ]);
+
+                    $uploadedFiles[] = $fileRecord;
+                }
+            }
+
             return response()->json([
                 'message' => 'Post creado exitosamente',
                 'data' => $post,
+                'files' => $uploadedFiles,
             ], 201);
         } catch (\Throwable $th) {
             return response()->json([
@@ -86,7 +127,27 @@ class PostController extends Controller
                 return response()->json(['message' => 'No autorizado para eliminar este post'], 403);
             }
 
+            //delete the files associated to post (if exists)
+            $files = $post->files; //we use the "posts-files" relation 
+
+            foreach($files as $file) {
+                //delete the file from storage system
+                $filePath = storage_path('app/public/' . str_replace('storage/', '', $file->path));
+
+                if(file_exists($filePath)) {
+                    unlink($filePath);
+                }
+
+                $file->forceDelete();
+            }
             $post->forceDelete();
+
+            // delete post folder if it's empty (optinal)
+            $folderPath = storage_path("app/public/posts/{$post->id}");
+            if (is_dir($folderPath) && count(scandir($folderPath)) == 2) { 
+                // Verifica que la carpeta esté vacía (solo contiene '.' y '..')
+                rmdir($folderPath); // Eliminar la carpeta si está vacía
+            }
 
             return response()->json([
                 'message' => 'Post eliminado exitosamente',
